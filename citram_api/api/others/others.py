9d191ced0b344d7651375a8c01983cc567376db3
jvicentem <j.vicentem@alumnos.urjc.es>
2020-01-01 20:47:45 +0100

    [FEAT] Transport card info implemented.

--- citram_api/api/others/others.py
+++ citram_api/api/others/others.py
@@ -1,4 +1,8 @@
+import xmltodict
+from zeep import Client
+
 from citram_api.constants.constants import Urls
+from citram_api.utils.custom_exceptions import NotEnoughParametersException
 from citram_api.utils.utils import common_request
 
 
@@ -7,3 +11,15 @@ def get_municipalities():
 
     return common_request(url_formatted)
 
+
+def get_ttp_card_info(ttp_number):
+    if ttp_number is not None:
+        client = Client(Urls.CITRAM_CARD_SERVICE.value)
+        result = client.service.ConsultaSaldo1(sNumeroTTP=ttp_number)
+    else:
+        raise NotEnoughParametersException('You must specify a transport card number.')
+
+    final_result = {'status': result['iCallLogField'],
+                    'card_info': xmltodict(result['sResulXMLField'])}
+
+    return final_result
