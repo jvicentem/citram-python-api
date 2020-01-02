d65e90ecd1fcc070716e795ff457d15b6ada1fbc
jvicentem <j.vicentem@alumnos.urjc.es>
2020-01-02 13:14:05 +0100

    [DEPLOY] Setup created

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='citram-python-api',
+    version='1.0.0',
+    author='José Vicente Mellado',
+    author_email='contact@jvm16.xyz ',
+    description='Regional Consortium of Transportation for Madrid API wrapper',
+    keywords='madrid transport api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jvicentem/citram-python-api',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=['zeep==3.4.0',
+                      'xmltodict==0.12.0',
+                      'requests==2.22.0']
+)
